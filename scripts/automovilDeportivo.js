03437557463224c8319387030206efa9a572ce1c
Laura López <lauralopez166@gmail.com>
2023-12-04 12:11:33 +0100

    acabado todo

--- scripts/automovilDeportivo.js
+++ scripts/automovilDeportivo.js
@@ -1,13 +1,14 @@
 import {Vehiculo} from './vehiculo.js';
 import {mostrarResultado} from './main.js';
 
+//Heredamos de la clase vehiculo la clase hija
 export class AutomovilDeportivo extends Vehiculo{
     #potenciaMotor
     constructor(modelo,marca,color, anioFabricacion, cilindrada, potenciaMotor){
         super(modelo,marca,color,anioFabricacion,cilindrada);
         this.#potenciaMotor = potenciaMotor;
     }
-
+    //Funcion propia de la clase hija
     activarModoDeportivo(){
         let mensaje = `<p>El modo deportivo ha sido activado</p>`;
         mostrarResultado(mensaje);
